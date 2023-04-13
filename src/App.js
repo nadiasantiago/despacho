190d5082c21b048a0d0ecb594a093abc1fd744a2
Fernando Lara <larafernandoagustin@gmail.com>
2023-04-13 01:01:05 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Header from './components/header/Header';
 import QuienesSom from './components/quienesSomos1/QuienesSom';
+
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
       <Header />
+
       <main>
         <QuienesSom />
+        
       </main>
+
+      <Footer/>
     </div>
   );
 }
