133eedd01c72fb596754d6190460e3b7d9828b3b
Nadia Santiago <nadusantiago@gmail.com>
2023-04-12 23:34:22 -0300

    mision y vision

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import QuienesSom from './components/quienesSomos1/QuienesSom';
+import QuienesSom2 from './components/quienesSomos2/QuienesSom2';
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-        <QuienesSom />
+        <div>
+          <QuienesSom />
+        </div>
+        <div>
+          <QuienesSom2 />
+        </div>
       </main>
     </div>
   );
