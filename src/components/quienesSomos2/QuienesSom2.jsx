d4f05194407a37ef3adff79311986718251d6a83
Nadia Santiago <nadusantiago@gmail.com>
2023-04-13 22:01:34 -0300

    mision y vision terminados

--- src/components/quienesSomos2/QuienesSom2.jsx
+++ src/components/quienesSomos2/QuienesSom2.jsx
@@ -5,21 +5,22 @@ import './QuienesSom2.css';
 const QuienesSom2=()=>{
     return(    
         <div className="quienes2Conteiner">
-            <div className="logo2">
-                <svg viewBox="0 0 273 556" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M213.502 105.979H225.98C226.064 106.832 227.219 116.407 235.64 120.434C236.823 120.994 241.865 123.421 247.441 121.447C253.384 119.341 255.468 113.793 255.835 112.806C258.398 106.006 255.243 97.5512 247.976 91.4172H25.0772C17.6698 94.9642 13.7547 102.272 15.2474 108.886C16.881 116.167 24.4011 119.287 24.9363 119.501C26.26 120.007 31.5833 121.927 37.2726 119.634C42.9338 117.367 46.7081 111.846 47.0742 105.499L58.0585 105.766C59.4105 116.54 54.1999 127.021 44.8491 132.382C33.8083 138.703 18.9089 136.809 9.27641 127.475C7.98081 126.195 -1.70787 116.327 0.263691 102.138C1.75645 91.2305 9.58634 81.8427 20.6553 77.5488H251.61C263.833 82.4027 271.889 93.4441 272.142 105.606C272.368 117.474 265.129 130.382 251.61 134.596C239.809 138.276 225.923 134.516 218.6 124.941C212.714 117.26 213.221 108.699 213.474 106.006L213.502 105.979Z" fill="white" fill-opacity="0.1"/>
-                <path d="M197.525 92.1785H181.71V440.233V515.22L62.9254 400.544L49.7462 413.27L187.358 546.154L197.525 556V92.1785Z" fill="white" fill-opacity="0.1"/>
-                <path d="M77.5769 103.915H162.007V189.657L148.736 175.689V116.171H93.0827V180.092L168.265 255.036C168.153 330.484 168.041 405.933 167.93 481.381L77.549 390.871V250.632L93.0827 266.788V383.074C113.31 403.549 133.565 424.024 153.793 444.499C153.486 383.551 153.206 322.603 152.899 261.627L77.549 183.738V103.886L77.5769 103.915Z" fill="white" fill-opacity="0.1"/>
-                <path d="M128.811 0V46.2224H86.4541L77.549 55.6H188.749L181.585 46.2224L143.681 45.501V0H128.811Z" fill="white" fill-opacity="0.1"/>
-                </svg>
-            </div>
+            
+            <svg className='logo2' viewBox="0 0 273 556" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M213.502 105.979H225.98C226.064 106.832 227.219 116.407 235.64 120.434C236.823 120.994 241.865 123.421 247.441 121.447C253.384 119.341 255.468 113.793 255.835 112.806C258.398 106.006 255.243 97.5512 247.976 91.4172H25.0772C17.6698 94.9642 13.7547 102.272 15.2474 108.886C16.881 116.167 24.4011 119.287 24.9363 119.501C26.26 120.007 31.5833 121.927 37.2726 119.634C42.9338 117.367 46.7081 111.846 47.0742 105.499L58.0585 105.766C59.4105 116.54 54.1999 127.021 44.8491 132.382C33.8083 138.703 18.9089 136.809 9.27641 127.475C7.98081 126.195 -1.70787 116.327 0.263691 102.138C1.75645 91.2305 9.58634 81.8427 20.6553 77.5488H251.61C263.833 82.4027 271.889 93.4441 272.142 105.606C272.368 117.474 265.129 130.382 251.61 134.596C239.809 138.276 225.923 134.516 218.6 124.941C212.714 117.26 213.221 108.699 213.474 106.006L213.502 105.979Z" fill="white" fill-opacity="0.1"/>
+            <path d="M197.525 92.1785H181.71V440.233V515.22L62.9254 400.544L49.7462 413.27L187.358 546.154L197.525 556V92.1785Z" fill="white" fill-opacity="0.1"/>
+            <path d="M77.5769 103.915H162.007V189.657L148.736 175.689V116.171H93.0827V180.092L168.265 255.036C168.153 330.484 168.041 405.933 167.93 481.381L77.549 390.871V250.632L93.0827 266.788V383.074C113.31 403.549 133.565 424.024 153.793 444.499C153.486 383.551 153.206 322.603 152.899 261.627L77.549 183.738V103.886L77.5769 103.915Z" fill="white" fill-opacity="0.1"/>
+            <path d="M128.811 0V46.2224H86.4541L77.549 55.6H188.749L181.585 46.2224L143.681 45.501V0H128.811Z" fill="white" fill-opacity="0.1"/>
+            </svg>
+            
             <div className="visionConteiner">
                 <h2>Visión</h2>
-                <p>Brindamos un servicio integral y de excelencia al cliente, adaptándonos a sus necesidades e innovando para generar nuevas y mejores soluciones. Queremos crear un lazo de confianza firme con las personas, donde nuestra prioridad es el bienestar común.</p>
+                <p>Ser referentes en la prestación de servicios jurídicos, brindando un servicio integral y de excelencia al cliente, adaptándose e innovando para generar nuevas y mejores soluciones a sus problemas. <br></br>
+                Crear un lazo de confianza firme con las personas, haciendo de sus necesidades nuestra principal prioridad.</p>
             </div>
             <div className="misionConteiner">
                 <h2>Misión</h2>
-                <p>Somos una herramienta esencial para la solución de sus problemas jurídicos, brindamos asesoramiento, representación y defensa en diferentes áreas legales. Trabajamos bajo los pilares de la empatía, el profesionalismo y la confianza.</p>
+                <p>Nuestra misión es proporcionar una solución integral para sus necesidades legales, permitiéndole al cliente tomar decisiones informadas y enfrentar los desafíos que se le presenten con confianza. Ser una herramienta esencial, dinámica y comprometida como respuesta en el asesoramiento, representación, defensa y resolución en las diferentes áreas del Derecho.</p>
             </div>
         </div>
     )
